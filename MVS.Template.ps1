56586bbdf2b0c53d981aa4f13e189f2fc7ed5da0
Matheus Souza <mvs@mvsouza.com>
2019-01-19 19:11:13 -0200

    Fixed install task.

--- MVS.Template.ps1
+++ MVS.Template.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [parameter(Mandatory=$true, Position=1)]
-    [ValidateSet("install", "clean")]
+    [ValidateSet("install")]
     [string]$action,
     [parameter(Mandatory=$false)]
     [switch]$all
@@ -11,27 +11,41 @@ process {
     $tasks.Add("install",@{
         description="Installs the template on dotnet";
         script = {
-            if (Test-Path temp) { rm temp -Force }
-            git clone https://github.com/mvsouza/MVS.Template.CSharp.git temp;
-            dotnet new -u mvs; 
-            if (Test-Path .\MVS.Template.CSharp.1.0.0.nupkg) { rm .\MVS.Template.CSharp.1.0.0.nupkg -Force }
-            dotnet clean .\content\MVS.Template.CSharp.sln ;
-            nuget pack .\MVS.Template.CSharp.nuspec; 
-            dotnet new -i .\MVS.Template.CSharp.1.0.0.nupkg
-        }
-    });
-    $tasks.Add("clean",@{
-        description="Installs the template on dotnet";
-        script = {
-            if (Test-Path temp) { rm temp -Force }
-            dotnet new -u mvs; 
-            if (Test-Path .\MVS.Template.CSharp.1.0.0.nupkg) { rm .\MVS.Template.CSharp.1.0.0.nupkg -Force }
-            rm content/* -Exclude .template.config
+            $task = $tasks.Get_Item("updateContent")
+            if($task){
+                Invoke-Command $task.script -ArgumentList (,$tasks);
+            }
+            $task = $tasks.Get_Item("buildInstall")
+            if($task){
+                Invoke-Command $task.script -ArgumentList (,$tasks);
+            }
         }
     });
 
-    $task = $tasks.Get_Item($action)
-    if ($task) {
-        Invoke-Command $task.script
+    $tasks.Add("updateContent",@{
+        description="Update the template content";
+        script = {
+            git clone https://github.com/mvsouza/MVS.Template.CSharp.git temp;
+            ls content  | ? {$_.Name -ne  ".template.config"} | %{rm $_.FullName -Recurse -Force}; #rm content/* -Exclude .template.config/* -Recurse;
+            mv .\temp\* .\content\;
+            rm temp -Force -Recurse
+        }
+    });
+
+    $tasks.Add("buildInstall",@{
+        description="Build and installs the template on dotnet";
+        script = {
+            #dotnet clean .\content\MVS.Template.CSharp.sln -ErrorAction SilentlyContinue;
+            dotnet new -u mvs; 
+            if (Test-Path .\MVS.Template.CSharp.1.0.0.nupkg) { rm .\MVS.Template.CSharp.1.0.0.nupkg -Force}
+            nuget pack .\MVS.Template.CSharp.nuspec;
+            dotnet new -i .\MVS.Template.CSharp.1.0.0.nupkg;
+        }
+    });
+    if ($action) {
+        $task = $tasks.Get_Item($action)
+        if($task){
+            Invoke-Command $task.script -ArgumentList (,$tasks);
+        }
     }
 }
